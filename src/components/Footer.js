55310a2ca87827170d816d805019f4774434aed1
Anthony Bussi <anthony.bussi@gmail.com>
2021-11-10 09:39:10 +0100

    Création du composant Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <small>Anthony BUSSI - L'école Multimédia - 2021</small>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import MovieCard from '../components/MovieCard';
 import axios from 'axios';
-
 
 import './style/Home.css';
 
@@ -49,6 +49,7 @@ const Home = () => {
                 </div>
                 
             </main>
+            <Footer />
         </div>
     );
 };

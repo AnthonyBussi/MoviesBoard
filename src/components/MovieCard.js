94e5f13ec4beb3f03c996ffa3613ca79bf9f6b2f
Anthony Bussi <anthony.bussi@gmail.com>
2021-11-10 16:01:28 +0100

    Création de la page Movie et ajouts css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './style/MovieCard.css';
 import FormatDate from '../services/FormatDate';
 
@@ -9,17 +9,18 @@ const Card = ( { movie } ) => {
 
     return (
         <div className="movie-card">
-            <NavLink to={`movies/${movieId}`} className="movie-link">
+            <Link to={`movies/${movieId}`} className="movie-link">
                 <h2 className="movie-title">{ movie.title }</h2>
                 <time className="movie-date">{FormatDate.DateForm(movie.release_date)}</time>
-                <img src={ movie.poster } alt={`affiche du film ${movie.title}`} className="movie-img" />
+                <img src={ movie.poster } alt={`Affiche du film ${movie.title}`} className="movie-img" />
                 <p className="movie-description">{ movie.description }</p>
                 <p className="movie-categories">{ movie.categories.join(' - ') }</p>
+                <p className="movie-categories">{ movie.actors.join(' - ') }</p>
                 <div className="movie-card-buttons">
                     <button type="button">Modifier</button>
                     <button type="button">Supprimer</button>
                 </div>
-            </NavLink>
+            </Link>
         </div>
     );
 }
--- src/views/Movie.js
+++ src/views/Movie.js
@@ -2,21 +2,45 @@ import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useParams } from 'react-router';
+import FormatDate from '../services/FormatDate';
 import axios from 'axios';
 
 
-import './style/Home.css';
+import './style/Movie.css';
 
 
 const Movie = () => {
-    const { id } = useParams()
+    const { id } = useParams();
     console.log(id);
+
+    const[movieData, setMovieData] = useState([]);
+
+    const URL = `http://localhost:3000/movies/${id}`;
+
+    useEffect(() => {
+        axios.get(URL)
+        .then((response) => setMovieData(response.data))
+        .catch((err) => console.log(err))},
+         [])
+        console.log(movieData);
     
     return (
-        <div className="details page">
+        <div className="movie page">
             <Header />
-            <main className="details-container">
-            </main>
+                <main className="detail-movie-container">
+                    <div className="detail-movie-container-info">
+                        <h2 className="detail-movie-title">{ movieData.title }</h2>
+                        <time className="detail-movie-date">{FormatDate.DateForm(movieData.release_date)}</time>
+                        <p className="detail-movie-description">{ movieData.description }</p>
+                    </div>
+                    <div className="detail-movie-container-img">
+                        <img src={ movieData.poster } alt={`Affiche du film ${movieData.title}`} className="detail-movie-img" />
+                        <div className="detail-movie-card-buttons">
+                            <button type="button">Modifier</button>
+                            <button type="button">Supprimer</button>
+                        </div>
+                    </div>    
+                </main>
             <Footer />
         </div>
     );
